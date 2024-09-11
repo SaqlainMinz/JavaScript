b683f820fc97c8560aeed93a295161be1794cd73
Saqlain Anish Minz <113945216+SaqlainMinz@users.noreply.github.com>
2024-09-11 17:14:00 +0000

    new update

--- 01_basic/03_conversionOperator.js
+++ 01_basic/03_conversionOperator.js
@@ -1,4 +1,4 @@
-let score ="56";
+let score = "56";
 
 console.log(typeof score);
 // conversion
@@ -11,3 +11,26 @@ console.log(typeof valueInNumber);
 let isLoggedIn = 1;
 let booleanValue = Boolean(isLoggedIn);
 console.log(booleanValue);
+
+// ***********************   Operations   **************************
+
+let value = 3;
+let negValue = -value;
+console.log(negValue);
+
+let a=10;
+let b=2;
+console.log(a+b);
+console.log(a-b);
+console.log(a*b);
+console.log(a/b);
+console.log(a%b);
+console.log(a**b);
+
+//concatination 
+let str1="hello";
+let str2 =" Saqlain"
+
+console.log(str1+str2);
+
+
