0be0ab6ccc3b98cdfef66614dbe378d9f58fbf77
Saqlain Anish Minz <113945216+SaqlainMinz@users.noreply.github.com>
2024-09-12 15:48:08 +0000

    numAndMath

---
+++ 01_basic/06_num_and_math.js
@@ -0,0 +1,44 @@
+const score =400;
+// console.log(score);
+
+const balance = new Number(100.5);
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed());
+
+const anotherNumber =10000000000;
+// console.log(anotherNumber.toLocaleString("en-IN"));
+
+const hundred =500;
+// console.log(typeof(hundred.toString()));
+
+
+// ++++++++++++++++++++ Math ++++++++++++++++++++++++++++
+
+console.log(Math.floor(10.5))
+console.log(Math.ceil(10.5))
+console.log(Math.round(10.5))
+console.log(Math.abs(-5))
+console.log(Math.max(1,2,3,4,5))
+console.log(Math.min(1,2,3,4,5))
+
+
+console.log(Math.random());
+
+//Rolling a dice 
+
+console.log(Math.floor(Math.random()*6)+1);
+
+
+//in between max min
+
+const max =20;
+const min =10;
+
+console.log(Math.floor(Math.random()*(max-min+1))+min);
+
+
+
+
+
