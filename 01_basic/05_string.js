c21f93c2d9776dccc582df1edde66b02ed6e712a
Saqlain Anish Minz <113945216+SaqlainMinz@users.noreply.github.com>
2024-09-12 14:38:35 +0000

    String

---
+++ 01_basic/05_string.js
@@ -0,0 +1,20 @@
+const name = "Saqlain";
+const age =20;
+
+//Old syntax
+console.log("hi my name is "+name+" and i am "+age+" old" );
+
+//mordern syntax
+console.log(`hi my name is ${name} and i am ${age} year's old`);
+
+
+const bottle = new String('Milton');
+
+console.log(bottle)
+console.log(bottle.toLowerCase());
+console.log(bottle.toUpperCase());
+console.log(bottle.indexOf('l'));
+console.log(bottle.charAt(4));
+console.log(bottle.slice(1,4));
+
+
