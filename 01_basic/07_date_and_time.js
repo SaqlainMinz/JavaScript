543640ab704741533de92153ea649d4626307a74
Saqlain Anish Minz <113945216+SaqlainMinz@users.noreply.github.com>
2024-09-13 02:06:40 +0000

    date and time

---
+++ 01_basic/07_date_and_time.js
@@ -0,0 +1,10 @@
+const date = new Date();
+console.log(date);
+console.log(date.toLocaleString());
+console.log(date.toString());
+console.log(date.getHours());
+
+
+const date1 = new Date(2024, 11, 17, 3, 24, 0);
+console.log(date1.toLocaleString());
+
