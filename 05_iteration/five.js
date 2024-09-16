24b6f86f430e151d2998961604f034fb34b8f90d
Saqlain Anish Minz <113945216+SaqlainMinz@users.noreply.github.com>
2024-09-16 17:45:46 +0000

    for each

--- 05_iteration/five.js
+++ 05_iteration/five.js
@@ -1,1 +1,30 @@
 const coding = ['js','cpp','swift','ruby','java'];
+
+// coding.forEach(function(value){
+//     console.log(value);
+// })
+
+
+// coding.forEach((value)=>{
+//     console.log(value);
+// })
+
+const myObj =[
+{
+    language : "javascript",
+    shortForm : "js",
+},
+{
+    language : "cpp",
+    shortForm : "c++",
+},
+{
+    language : "python",
+    shortForm : "py"
+}
+];
+
+myObj.forEach((value)=>{
+    console.log(value.language , value.shortForm);
+    
+})
