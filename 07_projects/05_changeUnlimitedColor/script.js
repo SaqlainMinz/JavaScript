2b131a0c57e50232fcd3a4f5670755703894c588
SaqlainMinz <officialsaqlainminz98@gmail.com>
2024-09-28 20:29:28 +0530

    js_timeInterval

---
+++ 07_projects/05_changeUnlimitedColor/script.js
@@ -0,0 +1,37 @@
+const start = document.querySelector("#start");
+const stop = document.querySelector("#stop");
+
+const randomColor = function () {
+  let hex = "0123456789ABCDEF";
+  let color = "#";
+
+  for (let i = 0; i < 6; i++) {
+    color += hex[Math.floor(Math.random() * 16)];
+  }
+  return color;
+};
+console.log(randomColor());
+
+
+let intervalId;
+
+
+const startChangingColor = function () {
+    if (!intervalId) {
+        
+        intervalId = setInterval(changeBgColor, 1000);
+    }
+
+  function changeBgColor() {
+    document.body.style.backgroundColor = randomColor();
+  }
+};
+
+
+const stopChangingColor = function () {
+  clearInterval(intervalId);
+  intervalId=null;
+};
+
+start.addEventListener("click", startChangingColor);
+stop.addEventListener("click", stopChangingColor);
